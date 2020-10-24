e86a0cd513c59a99520c2b27228a026502dec787
r2codes <59179357+r2codes@users.noreply.github.com>
2020-10-24 19:39:22 +0530

    Add files via upload

---
+++ Del4.c
@@ -0,0 +1,25 @@
+//prime number program
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,num,prime=0;;
+    printf("Enter the number to check it's prime or not : \n");
+    scanf("%d",&num);
+    for(i=2;i<=sqrt(num);i++)
+    {
+if(num%i==0)
+{
+prime=1;
+}
+    }
+    if(prime==0)
+    {
+        printf("It's not a  prime number.");
+    }
+ else
+ {
+     printf("It's a prime number.");
+ }
+
+}
